50c33164ffb214a961db449dd92f5bf320aa6371
rof1yev <rofiyevdilshod@gmail.com>
2026-03-19 21:52:57 +0500

    try/catch functionality added

--- components/home/trending-coins.tsx
+++ components/home/trending-coins.tsx
@@ -4,6 +4,7 @@ import { cn, formatCurrency } from "@/lib/utils";
 import { TrendingDown, TrendingUp } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { TrendingCoinsFallback } from "./fallback";
 
 const columns: DataTableColumn<TrendingCoin>[] = [
   {
@@ -64,26 +65,31 @@ const columns: DataTableColumn<TrendingCoin>[] = [
 ];
 
 const TrendingCoins = async () => {
-  const trendingCoins = await fetcher<{ coins: TrendingCoin[] }>(
-    "/search/trending",
-    undefined,
-    300,
-  );
+  let trendingCoins;
+
+  try {
+    trendingCoins = await fetcher<{ coins: TrendingCoin[] }>(
+      "/search/trending",
+      undefined,
+      300,
+    );
+  } catch (error) {
+    console.log("Error fetching trending coins:", error);
+    return <TrendingCoinsFallback />;
+  }
 
   return (
     <div id="trending-coins">
       <h4>Trending Coins</h4>
 
-      <div className="trending-coins">
-        <DataTable
-          data={trendingCoins.coins.slice(0, 6) || []}
-          columns={columns}
-          rowKey={(coin) => coin.item.id}
-          tableClassName="trending-coins-table"
-          headerCellClassName="py-3!"
-          bodyCellClassName="py-2!"
-        />
-      </div>
+      <DataTable
+        data={trendingCoins.coins.slice(0, 6) || []}
+        columns={columns}
+        rowKey={(coin) => coin.item.id}
+        tableClassName="trending-coins-table"
+        headerCellClassName="py-3!"
+        bodyCellClassName="py-2!"
+      />
     </div>
   );
 };
